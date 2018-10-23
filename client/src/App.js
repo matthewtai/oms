d63abb4c0b87c571b0af6f863481ebdf696883a7
wienerbrod <matthewtai@gmail.com>
2018-10-23 18:40:20 -0400

    fixednoodles

--- client/src/App.js
+++ client/src/App.js
@@ -1,71 +1,22 @@
-import React, { Component } from 'react';
-import _ from 'lodash';
-import axios from 'axios';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Main from "./pages/Main";
+// import Detail from "./pages/Detail";
+// import NoMatch from "./pages/NoMatch";
 
-import SearchBar from './components/SearchBar/SearchBar';
-import StockList from './components/StockList/StockList';
-import './App.css';
 
-class App extends Component {
-  constructor() {
-    super();
-
-    this.state = {
-      stocks: [],
-      term: null,
-      value: ''
-    };
-
-    this.handleClick = this.handleClick.bind(this);
-    this.handleChange = this.handleChange.bind(this);
-  }
-
-  handleChange(e) {
-    this.setState({
-      value: e.target.value
-    });
-  }
-
-  handleClick(e) {
-    if(e) e.preventDefault();
-    this.setState({
-      value: '',
-      term: this.state.value
-    });
-
-    let term = this.state.value;
-    const key = 'F41ON15LGCFM4PR7';
-    const url = `https://www.alphavantage.co/query?function=BATCH_STOCK_QUOTES&symbols=${term}&apikey=${key}`;
-
-    axios.get(url)
-    .then(res => {
-      console.log(res.data);
-      let stocks = _.flattenDeep( Array.from(res.data['Stock Quotes']).map((stock) => [{symbol: stock['1. symbol'], price: stock['2. price'], volume: stock['3. volume'], timestamp: stock['4. timestamp']}]) );
-      console.log(stocks);
-      this.setState((state, props) => {
-        return {
-          ...state,
-        stocks
-        }
-      })
-    })
-    .catch(error => console.log(error))
-  }
-
-  render () {
-    let stocks = this.state.stocks;
-    const value = this.state.value;
-
-    return (
-      <div className="App">
-        <h1 className="App__Title">Stock Search</h1>
-        <SearchBar value={ value }
-                   onChange={ this.handleChange }
-                   onClick={ this.handleClick }/>
-        <StockList stockItems={ this.state.stocks }/>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <Router>
+    <div>
+      <Switch>
+        <Route exact path="/" component={Main} />
+        {/* <Route exact path="/intraday" component={Main} /> */}
+        {/* <Route exact path="/books" component={Books} />
+        <Route exact path="/books/:id" component={Detail} />
+        <Route component={NoMatch} /> */}
+      </Switch>
+    </div>
+  </Router>
+);
 
 export default App;
