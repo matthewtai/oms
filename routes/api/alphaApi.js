c74006a32d38542d8c7547b95a76db650321c32b
william zhu <william_zhu@hotmail.ca>
2018-10-23 21:30:07 -0400

    test

--- routes/api/alphaApi.js
+++ routes/api/alphaApi.js
@@ -7,7 +7,7 @@ router.get("/search", (req, res) => {
   axios
     .get("https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=BA&apikey=query", { params:keys.alpha.api })
     .then( Response => {
-      console.log(Response.data.bestMatches[0]);
+      console.log(Response.data.bestMatches[0]['2. name']);
     })
     .catch(err => res.status(422).json(err));
 });
