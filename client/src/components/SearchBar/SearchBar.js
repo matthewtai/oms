531efa5afb00b2d7e71278858a62a3a49513c9c3
Matthew Tai <matthewtai@gmail.com>
2018-10-25 17:17:55 -0400

    Changes to SearchBar, using Fabric components, updated green/red % ch

--- client/src/components/SearchBar/SearchBar.js
+++ client/src/components/SearchBar/SearchBar.js
@@ -1,18 +1,27 @@
-import React from 'react';
-import './SearchBar.css';
+// import React from 'react';
+// import './SearchBar.css';
+// import { SearchBox } from 'office-ui-fabric-react/lib/SearchBox';
 
-function SearchBar(props) {
+// function SearchBar(props) {
 
-    return  (
-      <div className="SearchBar">
-        <form className="SearchBar_Form">
-          <input className="SearchBar__Input"
-                 value={ props.value }
-                 onChange={ props.onChange } />
-          <button className="SearchBar__Button" onClick={ props.onClick }>search</button>
-        </form>
-      </div>
-    );
-}
+//     return  (
+//       <div className="SearchBar">
+//         <form className="SearchBar_Form">
+//           <input className="SearchBar__Input"
+//                  value={ props.value }
+//                  onChange={ props.onChange } />
+//           <button className="SearchBar__Button" onClick={ props.onClick }>search</button>
+//         </form>
+//       </div>
+    
+//     //   <SearchBox
+//     //   placeholder="Ticker"
+//     //   onChange={props.value}
+//     //   onSearch={props.value}
+      
+//     // />
+    
+//     );
+// }
 
-export default SearchBar;
+// export default SearchBar;
