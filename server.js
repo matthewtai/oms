cb7f1b67a76b331df95aeaa63ed4d569f3232da1
Matthew Tai <matthewtai@gmail.com>
2018-11-08 16:26:43 -0500

    routing

--- server.js
+++ server.js
@@ -12,7 +12,10 @@ app.use(bodyParser.json());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
-
+//heroku routing
+app.get('*', (request, response) => {
+	response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
 // Add routes, both API and view
 //app.use(routes);
 require("./routes/api/users")(app);
