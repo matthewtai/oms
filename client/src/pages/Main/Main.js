acf54861fc47ca08487166d2524fb885945081ad
Chestirito <19494641+Chestirito@users.noreply.github.com>
2018-11-08 16:42:32 -0500

    auto refresh maybe works

--- client/src/pages/Main/Main.js
+++ client/src/pages/Main/Main.js
@@ -52,11 +52,37 @@ class Main extends Component {
     this.loadPortfolioStaging();
     this.handlePortfolioManager();
     this.handleAllHolding();
+    setTimeout(this.autoRefresh, 2000);
+    //this.autoRefresh();
+  }
+
+  autoRefresh = () => {
+    Promise.all([API.getPortfolios(), this.loadStagingData()]).then(res => {
+      //console.log(res[0]);
+      let refPortfolios = res[0].data;
+      let portfolios = this.state.data;
+      for(var i = 0; i < portfolios.length; i++){
+        refPortfolios[i].newWeight = portfolios[i].newWeight;
+        refPortfolios[i].changed = portfolios[i].changed; 
+        refPortfolios[i].shares_buy_sell = portfolios[i].shares_buy_sell;
+        refPortfolios[i].buy_or_sell = portfolios[i].buy_or_sell;
+        refPortfolios[i].old_weight = portfolios[i].old_weight;
+        refPortfolios[i].shares_owned = portfolios[i].shares_owned;
+      }
+      this.setState({
+        data: refPortfolios
+      });
+      this.setupCurrentCash();
+      console.log("doing!");
+    })
+    .catch(err => console.log(err));
+    
+    setTimeout(this.autoRefresh, 2000);
   }
 
   loadPortfolioStaging(){
     Promise.all([this.loadPortfolios(), this.loadStagingData()]).then(res => {
-      console.log(res);
+      //console.log(res);
       this.setupCurrentCash();
     })
     .catch(err => console.log(err));
@@ -169,7 +195,9 @@ class Main extends Component {
           ticker: res.data["Global Quote"]["01. symbol"],
           price: parseFloat(res.data["Global Quote"]["05. price"]).toFixed(2)
         });
+
         this.findHolding(this.state.ticker);
+
         this.setState((state, props) => {
           return {
             ...state,
@@ -204,12 +232,13 @@ class Main extends Component {
         this.setState({
           exchangerate: exchangerate[0]["5. Exchange Rate"]
         });
+        this.handleCurrentWeight();
       })
       .catch(err => console.log(err));
   };
 
   handleSubmit = () => {
-    console.log(this.state.value)
+    //console.log(this.state.value)
     this.performSearch(this.state.value);
     this.handleAlphaApi(this.state.value);
   };
@@ -219,7 +248,7 @@ class Main extends Component {
     const holdings = this.state.holdingsData;
     const portfolios = this.state.data
     portfolios.push(...holdings)
-    console.log(portfolios)
+    //console.log(portfolios)
     portfolios.map(element => {
       if (element.changed) {
         this.handleSaveStages(element);
@@ -266,7 +295,7 @@ class Main extends Component {
 
   calculateShares = props => {
     //event.preventDefault();
-    console.log(props);
+    //console.log(props);
     const portfolios = props.original.holdings? this.state.holdingsData : this.state.data;
     const index = portfolios.findIndex(element => {
       return element.id === props.row.id;
@@ -318,6 +347,7 @@ class Main extends Component {
       return element.id === props.row.id;
     });
     return portfolios[index].newWeight;
+    //this.setState
   };
 
   testing = () => {
@@ -367,7 +397,7 @@ else{
    
     API.aggregateHoldings()
       .then(res => {
-        console.log(res)
+        //console.log(res)
         this.setupHoldingsData(res.data);
       })
       .catch(err => console.log(err));
@@ -379,7 +409,7 @@ else{
      
       API.aggregateHoldings()
         .then(res => {
-          console.log(res)
+          //console.log(res)
           this.setupHoldingsData(res.data);
         })
         .catch(err => console.log(err));
@@ -387,7 +417,7 @@ else{
   };
 
   setupHoldingsData = data => {
-    console.log(data)
+    //console.log(data)
     data.map(element => {
       element.newWeight = "";
       element.changed = false;
@@ -412,16 +442,21 @@ else{
   };
 
   //old weight
-  handleCurrentWeight = props => {
+  handleCurrentWeight = () => {
     const portfolios = this.state.data;
-    const index = portfolios.findIndex(element => {
-      return element.id === props.row.id;
-    });
-    const shares = portfolios[index].shares_owned;
-    const nav = portfolios[index].NAV;
-    let currentWeight = (((shares * this.state.price * this.state.exchangerate) / nav) *100).toFixed(2);
-
-    return (portfolios[index].old_weight = currentWeight);
+    // const index = portfolios.findIndex(element => {
+    //   return element.id === props.row.id;
+    // });
+    portfolios.map(element => {
+      const shares = element.shares_owned;
+      const nav = element.NAV;
+      let currentWeight = (((shares * this.state.price * this.state.exchangerate) / nav) *100).toFixed(2);
+      element.old_weight = currentWeight;
+    })
+    // const shares = portfolios[index].shares_owned;
+    // const nav = portfolios[index].NAV;
+    // let currentWeight = (((shares * this.state.price * this.state.exchangerate) / nav) *100).toFixed(2);
+    this.setState({data : portfolios});
   };
   tickerClickSearch = (props) => {
     const value = props.value
@@ -545,9 +580,9 @@ else{
                     {
                       Header: "Current Weight(%)",
                       accessor: "old_weight",
-                      Cell: props => {
-                        return <span>{this.handleCurrentWeight(props)}</span>;
-                      },
+                      // Cell: props => {
+                      //   return <span>{this.handleCurrentWeight(props)}</span>;
+                      // },
                       filterable: false,
                       maxWidth: 200
                     },
