3c5e6cee2d49cb520fcdff42156fb10806389f18
Matthew Tai <matthewtai@gmail.com>
2018-11-08 17:42:04 -0500

    hey

--- server.js
+++ server.js
@@ -5,13 +5,17 @@ const apiRoutes = require("./routes/api/alphaApi");
 const app = express();
 const PORT = process.env.PORT || 3001;
 var db = require("./models");
+const path = require('path');
 // Define middleware here
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  // app.use(express.static("client/build"));
+  app.use(express.static(path.join(__dirname, 'client/build')));
+
 }
+
 
 // Add routes, both API and view
 //app.use(routes);
