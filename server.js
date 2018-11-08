63baa3c6b34f26f62e4804f576d0021473e96ea2
Matthew Tai <matthewtai@gmail.com>
2018-11-08 17:21:16 -0500

    route

--- server.js
+++ server.js
@@ -11,7 +11,8 @@ app.use(bodyParser.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   // app.use(express.static("client/build"));
-  app.use(express.static("client"))
+  app.use(express.static(path.join(__dirname, 'client/build')));
+
 }
 
 // Add routes, both API and view
