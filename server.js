6e9a2c9e0ad9dea69b1c491a33bae4c5dd1e93c7
Matthew Tai <matthewtai@gmail.com>
2018-11-08 17:02:04 -0500

    routing

--- server.js
+++ server.js
@@ -10,9 +10,11 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  // app.use(express.static("client/build"));
+  app.use(express.static(__dirname))
+} else { // For development
+  app.use(express.static(__dirname + '/build'))
 }
-//heroku routing
 
 // Add routes, both API and view
 //app.use(routes);
